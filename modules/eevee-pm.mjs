77b6a1dddfb810d81692ff5190c32a9a1a31c40f
Edwin <pers.edwin@gmail.com>
2020-05-12 22:21:27 -0400

    start function

--- modules/eevee-pm.mjs
+++ modules/eevee-pm.mjs
@@ -6,9 +6,11 @@ const ident = 'eevee-pm';
 const debug = true;
 
 import { default as clog } from 'ee-log';
-
+import { default as child_process } from 'child_process';
+import { default as fs } from 'fs';
 import { ipc, lockPidFile, handleSIGINT } from '../lib/common.mjs';
 
+// Create and lock a pid file at /tmp/eevee/proc/eevee-pm.pid
 lockPidFile(ident);
 
 // Print every message we receive if debug is enabled
@@ -26,3 +28,23 @@ ipc.on('start', () => {
 process.on('SIGINT', () => {
   handleSIGINT(ident, ipc);
 });
+
+function start(request) {
+  if (debug) clog.debug('Start request received: ', request);
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  const out = fs.openSync(`../log/${request.ident}.log`, 'a');
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  const err = fs.openSync(`../log/${request.ident}.log`, 'a');
+  const child = child_process.fork(`./${request.ident}.mjs`, {
+    detached: true,
+    stdio: ['ignore', out, err, 'ipc'],
+  });
+  child.disconnect();
+  child.unref();
+}
+
+setTimeout(() => {
+  start({
+    ident: 'test1',
+  });
+}, 2000);

9195a9a1eb635830c412e537663bd5921fc7d5ee
Edwin Pers <pers.edwin@gmail.com>
2021-03-26 23:29:23 +0000

    start work on watchdog module

---
+++ modules/watchdog.mjs
@@ -0,0 +1,36 @@
+'use strict';
+
+// Watchdog module
+// Periodically pings modules, if they don't answer then we try to restart them
+
+import { default as clog } from 'ee-log';
+import { ipc, lockPidFile, handleSIGINT, getConfig, getGlobalConfig, getDirName, readableTime } from '../lib/common.mjs';
+
+const debug = true;
+
+const __dirname = getDirName();
+
+var moduleIdent = 'watchdog';
+
+lockPidFile(moduleIdent);
+
+const config = getConfig(moduleIdent);
+config.global = getGlobalConfig();
+if (debug) clog.debug(config);
+
+// Print every message we receive if debug is enabled
+if (debug) {
+  ipc.subscribe(`${moduleIdent}.#`, (data, info) => {
+    clog.debug('incoming IPC message: ', info, data.toString());
+  });
+}
+
+// Things that need to be done once the ipc is "connected"
+ipc.on('start', () => {
+  if (debug) clog.debug('IPC "connected"');
+  if (process.send) process.send('ready');
+});
+
+process.on('SIGINT', () => {
+  handleSIGINT(moduleIdent, ipc);
+});

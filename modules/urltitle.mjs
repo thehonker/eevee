d1c148220c1bd9ffde7e5f7b00def47c726a8800
Edwin Pers <pers.edwin@gmail.com>
2021-08-06 00:46:09 +0000

    remove debug stuff

--- modules/urltitle.mjs
+++ modules/urltitle.mjs
@@ -54,27 +54,25 @@ youtube.setKey(config.youtube.api_key);
 
 ipc.subscribe('_broadcast.incomingMessage.#', (data) => {
   const request = JSON.parse(data);
-  if (request.channel === '#botspam') {
-    const httpsRegex = new RegExp('https?\\S+', 'g');
-    if (httpsRegex.test(request.text)) {
-      if (debug) clog.debug('URL received:', request);
-      const url = request.text.match(httpsRegex);
-      if (debug) clog.debug('URL', url);
-      fetchTitle(url)
-        .then((titleString) => {
-          const reply = {
-            target: request.channel,
-            text: titleString,
-          };
-          if (debug) clog.debug(`Sending reply to: ${request.replyTo}.outgoingMessage`, reply);
-          ipc.publish(`${request.replyTo}.outgoingMessage`, JSON.stringify(reply));
-          return;
-        })
-        .catch((error) => {
-          clog.error(error);
-          return;
-        });
-    }
+  const httpsRegex = new RegExp('https?\\S+', 'g');
+  if (httpsRegex.test(request.text)) {
+    if (debug) clog.debug('URL received:', request);
+    const url = request.text.match(httpsRegex);
+    if (debug) clog.debug('URL', url);
+    fetchTitle(url)
+      .then((titleString) => {
+        const reply = {
+          target: request.channel,
+          text: titleString,
+        };
+        if (debug) clog.debug(`Sending reply to: ${request.replyTo}.outgoingMessage`, reply);
+        ipc.publish(`${request.replyTo}.outgoingMessage`, JSON.stringify(reply));
+        return;
+      })
+      .catch((error) => {
+        clog.error(error);
+        return;
+      });
   }
 });
 
@@ -85,15 +83,6 @@ function fetchTitle(url) {
     if (ytid) {
       youtube.getById([ytid], (error, response) => {
         if (error) return reject(error);
-        clog.debug(response);
-        clog.debug(
-          response.items[0].snippet.title,
-          response.items[0].snippet.publishedAt,
-          response.items[0].statistics.viewCount,
-          response.items[0].statistics.likeCount,
-          response.items[0].statistics.dislikeCount,
-          response.items[0].statistics.commentCount,
-        );
         const title = response.items[0].snippet.title;
         const date = response.items[0].snippet.publishedAt;
         const views = response.items[0].statistics.viewCount;
@@ -118,11 +107,9 @@ function fetchTitle(url) {
         },
         (error, response) => {
           if (error) return reject(error);
-          clog.debug(response.body);
           const titleRegex = new RegExp('<title.*>(.*)<.*\\/title>');
           const title = response.body.match(titleRegex);
           if (title) {
-            clog.debug(title[1]);
             return resolve(`[ ${ircColor.blue(title[1])} ]`);
           }
         },

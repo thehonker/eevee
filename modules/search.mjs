6c9629a70e1ca48317e83d4427207f48a68af8dd
Edwin Pers <pers.edwin@gmail.com>
2022-09-15 15:51:03 +0000

    truncate ud output to one line

--- modules/search.mjs
+++ modules/search.mjs
@@ -480,7 +480,7 @@ function urbanDictSearch(request) {
         clog.debug('Recent results', recentResults);
       }
       clog.debug(selectedResult);
-      outputString = `Found ${request.args}: ${ircColor.blue(selectedResult.definition)}`;
+      outputString = `Found ${request.args}: ${ircColor.blue(selectedResult.definition.split(/[\n|\r\n]/)[0])}`;
       const reply = {
         target: request.channel,
         text: outputString,
